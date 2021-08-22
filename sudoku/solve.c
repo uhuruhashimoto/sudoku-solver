92487db6a906350f3e1e63a3d279ce5f6a6f63ec
Uhuru G. Hashimoto <f003mfq@plank.thayer.dartmouth.edu>
2021-08-22 18:00:25 -0400

    modified solve; needs unit tests

--- sudoku/solve.c
+++ sudoku/solve.c
@@ -8,20 +8,14 @@
 #include <stdlib.h>
 #include "board.h"
 
-void solve();
-board *get_solved_board(board *);
-
 // solve - scans, solves, and prints board
-void solve() {
-    board *unsolved = scan_board(stdin);
-    board *solved = get_solved_board(unsolved);
-    print_board(solved);
-}
+void solve(board puzzle) {
+    board_scan(puzzle, stdin);
 
-// helper - calls backtrack and returns solved board
-board *get_solved_board(board *puzzle) {
-    editable_spots = get_editable_spots(puzzle);
+    editable_spots_t spots = board_editable_spots(puzzle);
     int *num_sol = malloc(sizeof(int));
-    backtrack(puzzle, editable_spots, (const int) 1, num_sol);
+    backtrack(puzzle, spots, (const int) 1, num_sol);
+    
+    board_print(puzzle);
     free(num_sol);
 }
